dfdcc9118374be65606c136d363393047125e93f
JazzZhao <fue7856321@163.com>
2022-03-30 16:20:30 +0800

    残差模型

--- main.py
+++ main.py
@@ -4,11 +4,31 @@ import torch
 from torch import nn
 import torchvision
 from torchvision import transforms
+from torch.nn import functional as F
 from torch.utils import data
 from tqdm import tqdm
 from IPython import display
 import matplotlib.pyplot as plt
 import utils
+
+class Residual (nn.Module):
+    def __init__(self, input_channels, num_channels, strides=1, use_1x1conv=False):
+        super().__init__()
+        self.conv1 = nn.Conv2d(input_channels, num_channels, kernel_size=3, padding=1, stride=strides)
+        self.conv2 = nn.Conv2d(num_channels, num_channels, kernel_size=3, padding=1)
+        if use_1x1conv :
+            self.conv3 = nn.Conv2d(input_channels, num_channels, kernel_size=1, stride=strides)
+        else:
+            self.conv3 = None
+        self.bn1 = nn.BatchNorm2d(num_channels)
+        self.bn2 = nn.BatchNorm2d(num_channels)
+    def forward (self, X):
+        Y = F.relu(self.bn1(self.conv1(X)))
+        Y = self.bn2(self.conv2(Y))
+        if self.conv3:
+            X = self.conv3(X)
+        Y += X
+        return F.relu(Y)
 
 def nin_block(in_channels,out_channels,  kernel_size, strides, padding):
     return nn.Sequential(
@@ -26,33 +46,59 @@ def train_ch6(net , train_iter, test_iter, num_epochs, lr, device):
     print('training on', device)
     loss = nn.CrossEntropyLoss()
     optimizer = torch.optim.SGD(net.parameters(), lr=lr)
+    num_batches = len(train_iter)
+    animator = utils.Animator(xlabel='epoch', xlim=[1, num_epochs],
+                            legend=['train loss', 'train acc', 'test acc'])
     for epoch in tqdm(range(num_epochs)):
         net.train()
-        metric = utils.Accumulator(2)
+        metric = utils.Accumulator(3)
         for i, (X, y) in enumerate(train_iter):
             optimizer.zero_grad()
             X, y = X.to(device), y.to(device)
-            l = loss(net(X), y)
+            y_hat = net(X)
+            l = loss(y_hat, y)
             l.backward()
             optimizer.step()
-        print(l)
+            with torch.no_grad():
+                metric.add(l* X.shape[0], utils.accuracy(y_hat, y), X.shape[0])
+            train_l = metric[0] / metric[2]
+            train_acc = metric[1] / metric[2]
+            if (i + 1) % (num_batches // 5) == 0 or i == num_batches - 1:
+                animator.add(epoch + (i + 1) / num_batches,
+                             (train_l, train_acc, None))
+        test_acc = utils.evaluate_accuracy_gpu(net, test_iter)
+        animator.add(epoch + 1, (None, None, test_acc))
+    print(f'loss {train_l:.3f}, train acc {train_acc:.3f}, '
+          f'test acc {test_acc:.3f}')    
+
+def resnet_block(input_channels, num_channels, num_residuals, first_block = False):
+    blk = []
+    for i in range(num_residuals):
+        if i==0 and not first_block:
+            #第一个长宽减半
+            blk.append(Residual(input_channels, num_channels,strides=2, use_1x1conv=True))
+        else:
+            blk.append(Residual(num_channels, num_channels))
+    return blk    
 
 def main():
-    net = nn.Sequential(
-        nin_block(1, 96, kernel_size=11, strides=4, padding=0),
-        nn.MaxPool2d(3, stride=2),
-        nin_block(96, 256, kernel_size=5, strides=1, padding=2),
-        nn.MaxPool2d(3, stride=2),
-        nin_block(256, 384, kernel_size=3, strides=1, padding=1),
-        nn.MaxPool2d(3, stride=2),
-        nn.Dropout(0.5),
-        # 标签类别数是10
-        nin_block(384, 10, kernel_size=3, strides=1, padding=1),
-        nn.AdaptiveAvgPool2d((1, 1)),
-        # 将四维的输出转成二维的输出，其形状为(批量大小,10)
-        nn.Flatten())
-    train_iter, test_iter = utils.load_data_fashion_mnist(64, resize=224)
-    train_ch6(net, train_iter, test_iter, 10, 0.1, utils.try_gpu())
+    b1 = nn.Sequential(
+        nn.Conv2d(1, 64, kernel_size=7, stride=2, padding=3),
+        nn.BatchNorm2d(64), nn.ReLU(),
+        nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+    )
+    b2 = nn.Sequential(*resnet_block(64, 64, 2, True))
+    b3 = nn.Sequential(*resnet_block(64, 128, 2))
+    b4 = nn.Sequential(*resnet_block(128, 256, 2))
+    b5 = nn.Sequential(*resnet_block(256, 512, 2))
+    net = nn.Sequential(b1, b2, b3, b4, b5,
+        nn.AdaptiveAvgPool2d((1,1)),
+        nn.Flatten(), nn.Linear(512, 10)
+    )
+    lr, num_epochs, batch_size = 0.05, 10, 256
+    train_data, test_data = utils.load_data_fashion_mnist(batch_size, resize=96)
+    train_ch6(net, train_data, test_data, num_epochs, lr, utils.try_gpu())
+
 
 if __name__ == "__main__":
     main()
