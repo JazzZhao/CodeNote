cbea0393d34efd875e2ec2976589d25c3b36bf3a
JazzZhao <fue7856321@163.com>
2022-05-24 13:54:58 +0800

    增加音乐播放功能

--- miniprogram-1/pages/voice/voice.js
+++ miniprogram-1/pages/voice/voice.js
@@ -1,25 +1,46 @@
 // pages/voice/voice.js
+const app = getApp();
+//引入插件：微信同声传译
+const plugin = requirePlugin('WechatSI');
+//获取全局唯一的语音识别管理器recordRecoManager
+const manager = plugin.getRecordRecognitionManager();
+const musicTool = require("../../utils/QQMusicPlugin/qqMusicTools.js")
+const backgroundAudioManager = wx.getBackgroundAudioManager()
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    anmationShow: false
+    handle: false,
+    content:'你好，我的朋友，我是小龙',
+    src:''
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad(options) {
-
+    //识别语音
+    this.initRecord();
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady() {
-
+    //创建内部audio上下文
+    this.innerAudioContext = wx.createInnerAudioContext()
+    this.innerAudioContext.onError(function(res){
+      console.log(res);
+      wx.showToast({
+        title: '语音播放失败',
+        icon:'none'
+      })
+    })
+    //文字转语音(欢迎语)
+    var that = this;
+    that.wordYun();
   },
 
   /**
@@ -40,7 +61,7 @@ Page({
    * 生命周期函数--监听页面卸载
    */
   onUnload() {
-
+    backgroundAudioManager.stop();
   },
 
   /**
@@ -64,85 +85,120 @@ Page({
 
   },
 
+  //按钮
+  buttonHandel: function(event){
+    backgroundAudioManager.stop();
+    var text = event.currentTarget.dataset.value;
+    console.log(text)
+    this.setData({
+        content: text
+      })
+    this.wordYun();
+  },
+
   //按住按钮
   startHandel: function () {
-    this.setData({
-      sayimg: '../resource/image/maikefeng.jpg',
-      anmationShow: true,
+    backgroundAudioManager.stop();
+    // 语音开始识别
+    manager.start({
+      lang: 'zh_CN',// 识别的语言，目前支持zh_CN en_US zh_HK sichuanhua
     })
-    console.log("开始")
-    wx.getRecorderManager().start({
-      duration: 10000
-    })
-    const self = this
-    setTimeout(function () {
-      self.setData({
-        sayimg: '../resource/image/maikefeng.jpg',
-        anmationShow: false
-      })
-    }, 10000);
   },
   //松开按钮
   endHandle: function () {
-    // clearTimeout()
-    this.setData({
-      sayimg: '../resource/image/maikefeng.jpg', //图片样式
-      anmationShow: false,
-    })
-    console.log("结束")
-    const recorderManager = wx.getRecorderManager()
-    //录音停止函数
-    var that = this;
-    wx.getRecorderManager().onStop((res) => {
-      if (!this.data.inpvalue) {
-        wx.showLoading({
-          title: '语音识别中'
-        })
-      }
-      const {
-        tempFilePath
-      } = res; //这里松开按钮 会返回录音本地路径
-      //上传录制的音频到服务器
-      wx.uploadFile({
-        url: '接口地址' + api.voice, //接口地址
-        name: 'file', //上传文件名
-        filePath: tempFilePath,
-        success: function (res) { //后台返回给前端识别后的文字
-          var model = res.data
-          var modeljson = JSON.parse(model)
-          if (modeljson.status_code == 500) {
-            wx.showToast({
-              title: '语音转换失败',
-              image: '../resource/image/maikefeng.jpg'
-            })
-            return false;
-          }
-          if (modeljson.meta.status_code === 200 && !modeljson.data.err_msg) {
-            var saymessage = modeljson.data.message;
-            wx.setStorageSync('sayinfo', saymessage)
-            that.setData({
-              inpvalue: saymessage
-            })
-            setTimeout(() => {
-              wx.navigateTo({
-                url: '../resource/image/maikefeng.jpg'
-              })
+    // 语音结束识别
+    manager.stop();
+  },
 
-            }, 2000)
-            setTimeout(() => {
-              wx.hideLoading();
-            }, 100)
-          } else if (modeljson.data.err_msg) {
-            wx.showToast({
-              title: '请大声说话',
-              image: '../resource/image/maikefeng.jpg'
-            })
-            return false;
-          }
-        }
+  // 文字转语音
+  wordYun:function (e) {
+    var that = this;
+    var content = this.data.content;
+    plugin.textToSpeech({
+      lang: "zh_CN",
+      tts: true,
+      content: content,
+      success: function (res) {
+        console.log(res);
+        console.log("succ tts", res.filename);
+        that.setData({
+          src: res.filename
+        })
+        that.yuyinPlay();
+      },
+      fail: function (res) {
+        console.log("fail tts", res)
+      }
+    })
+  },
+  //播放语音
+  yuyinPlay: function (e) {
+    if (this.data.src == '') {
+      console.log(暂无语音);
+      return;
+    }
+    this.innerAudioContext.src = this.data.src //设置音频地址
+    this.innerAudioContext.play(); //播放音频
+  },
+ 
+  // 结束语音
+  end: function (e) {
+    this.innerAudioContext.pause();//暂停音频
+  },
+
+  //识别语音 -- 初始化
+  initRecord: function () {
+    const that = this;
+    // 有新的识别内容返回，则会调用此事件
+    manager.onRecognize = function (res) {
+      console.log(res)
+    }
+    // 正常开始录音识别时会调用此事件
+    manager.onStart = function (res) {
+      console.log("成功开始录音识别", res)
+    }
+    // 识别错误事件
+    manager.onError = function (res) {
+      console.error("error msg", res)
+    }
+    //识别结束事件
+    manager.onStop = function (res) {
+      console.log('..............结束录音')
+      console.log('录音临时文件地址 -->' + res.tempFilePath); 
+      console.log('录音总时长 -->' + res.duration + 'ms'); 
+      console.log('文件大小 --> ' + res.fileSize + 'B');
+      console.log('语音内容 --> ' + res.result);
+      var text = '';
+      if (res.result == '') {
+        // wx.showModal({
+        //   title: '提示',
+        //   content: '听不清楚，请重新说一遍！',
+        //   showCancel: false,
+        //   success: function (res) {}
+        // })
+        // return;
+        text = '听不清楚，请重新说一遍！'
+      }else{
+          text = res.result
+      }
+      that.setData({
+        content: text
+      })
+      // that.wordYun();
+      that.playmusic()
+    }
+  },
+
+  //音乐部分
+  playmusic:function(){
+    const that = this;
+    musicTool.searchMusic(1, that.data.content).then(function(searchRes) {
+      musicTool.playMusic(searchRes.id).then(function(res){
+        backgroundAudioManager.title = searchRes.name
+        backgroundAudioManager.epname = searchRes.name
+        // // 设置了 src 之后会自动播放
+        backgroundAudioManager.src = res.url
       })
     })
-    //触发录音停止
-    wx.getRecorderManager().stop()
   }
 })
